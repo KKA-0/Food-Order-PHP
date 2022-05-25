bdc9d534103d9a77db9c46f6cb071199c47eb288
Karan Agarwal <karanyobro@gmail.com>
2022-05-26 00:05:09 +0530

    admin panel

---
+++ admin/index.php
@@ -0,0 +1,38 @@
+<html>
+    <head>
+        <title> Food Order Website - Home Page</title>
+    </head>
+
+    <body>
+        <h1> Admin Panel </h1>
+
+    <!-- Mean Section Starts -->
+    <div class="menu">
+        Menu Goes Here
+
+    </div>
+
+
+    <!-- Mean Section End -->
+
+    <!-- Mean Content Starts -->
+    <div class="main-content">
+        Menu Content Goes Here
+        
+    </div>
+
+
+    <!-- Mean Content End -->
+
+    <!-- Mean Footer Starts -->
+    Footer Goes Here
+    <div class="Footer">
+        
+    </div>
+
+
+    <!-- Mean Footer End -->
+
+    </body>
+    
+</html>
