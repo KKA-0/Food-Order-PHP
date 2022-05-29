b9dff5eaf5f3e41cb7d1a9f575de84c9abba746a
Karan Agarwal <karanyobro@gmail.com>
2022-05-29 16:19:16 +0530

    Update delete-admin.php

--- admin/delete-admin.php
+++ admin/delete-admin.php
@@ -1,9 +1,27 @@
 <?php
-
+    //include constants.php dile here
+    include('../config/constants.php');
     // 1. get the ID of admin to be deleted
-       echo $id = $_GET['id'];
+    $id = $_GET[ 'id' ];
     //2. create sql query to delete admin
-            $sql
-    //3. redirect to manage admin page with message (success)
+    $sql = "DELETE FROM tbl_admin WHERE id=$id";
+
+    //execute the query
+    $res = mysqli_query($conn, $sql);
+
+
+    //check whether the query exected successfully or not
+    if($res==TRUE)
+    {
+        //query exected successfully and admin deleted
+        echo "admin deleted";
+    }
+    else
+    {
+        //failed to delete admin
+        echo "failed the delete admin";
+    }
+//3. redirect to manage admin page with message (success)
 
 ?>
+<?php include('partials/footer.php'); ?>
